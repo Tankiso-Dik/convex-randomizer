9e7b345fa5c3d78c01008531865d8363c10cab4f
Tankiso-Dik <109304740+Tankiso-Dik@users.noreply.github.com>
2025-08-13 11:00:26 +0200

    Remove AuthGate wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import "./globals.css";
 import { ReactNode } from "react";
-import AuthGate from "./components/AuthGate";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AuthGate>{children}</AuthGate>
+        {children}
       </body>
     </html>
   );
